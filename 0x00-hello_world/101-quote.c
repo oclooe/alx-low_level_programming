e5fa9b7facd6d372047e71e4728132318cc0817a
oclooe <oclooe12@gmail.com>
2023-02-17 07:13:23 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,8 @@
 /**
  * main - A program that print a lne to the standard error
  * Return 1 (Success)
- * int main(void)
+ */
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
