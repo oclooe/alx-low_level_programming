a1c8a58bfeb2a1428e532a90cd77924d830eb33e
oclooe <oclooe12@gmail.com>
2023-03-15 13:37:12 -0700

    task 0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - prints/writes c
- * @c: The character to print
- * Return: 1, success, -1 error
- */
+* _putchar - prints/writes c
+* @c: The character to print
+* Return: 1, success, -1 error
+*/
 
 int _putchar(char c)
 {
