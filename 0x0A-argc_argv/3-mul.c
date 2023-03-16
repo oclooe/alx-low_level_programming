a75b4a7b88fc877d914bfe24617f92aeaadf6990
oclooe <oclooe12@gmail.com>
2023-03-16 01:46:50 -0700

    task2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 
@@ -17,4 +18,13 @@ if (argc < 3)
 {
 printf("Error\n");
 return (1);
+}
+
+
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x * y);
+
+
+return (0);
 }
