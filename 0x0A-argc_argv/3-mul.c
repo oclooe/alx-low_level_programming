d14b628b6897b3412766b47e20cc49eb765c093a
oclooe <oclooe12@gmail.com>
2023-03-16 00:51:25 -0700

    task1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+
+/**
+* main - multiplies two numbers.
+* @argc: argument count
+* @argv: arguments
+*
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+int x, y;
+
+
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
